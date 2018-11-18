a6e0f734300cb6ba59f1525fdb51b1306cbe12fc
Ivan Golikov <aeternass@gmail.com>
2018-11-18 20:19:52 +0300

    Formatted Appjs

--- App.js
+++ App.js
@@ -1,34 +1,33 @@
 //  Ivan Golikov 2018
 
 import React, {Component} from 'react';
-import {Alert, StyleSheet, Button, View} from 'react-native';
+import {Alert, NavigateIOS, StyleSheet, Button, View} from 'react-native';
 
 export default class App extends Component {
-	_onPressButton() {
-		Alert.alert("Should navigate to next screen")
-	}
+  _onPressButton() {
+    Alert.alert('Should navigate to next screen');
+  }
 
-	render() {
-		return (
-			<View style={styles.container}>
-			  <View style={styles.buttonContainer}>
-			    <Button
-			      onPress={this._onPressButton}
-			      title="Proceed to translation screen"
-			    />
-			  </View>
-			</View>
-		)
-	}
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="Proceed to translation screen"
+          />
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-   flex: 1,
-   justifyContent: 'center',
+    flex: 1,
+    justifyContent: 'center',
   },
   buttonContainer: {
-    margin: 20
+    margin: 20,
   },
-});	
-
+});
