233d075ceed5df178bf6fcbe17fbe1df8a810a0e
Ivan Golikov <aeternass@gmail.com>
2018-11-19 11:41:47 +0300

    Try to add additional npm install

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -o pipefail && cd ios && bundle install && \
+set -o pipefail && npm install && cd ios && bundle install && \
 gem install fastlane --no-rdoc --no-ri --no-document && \
 bundle exec pod install && \
 cd ../node_modules/react-native/third-party/glog-0.3.5 && \
