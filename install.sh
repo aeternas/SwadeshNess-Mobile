ac9124a9aa94cb1baecc85257d397c7dfdb5c145
Ivan Golikov <aeternass@gmail.com>
2018-11-24 14:21:07 +0300

    Removed install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -o pipefail && npm install && cd ios && bundle install && \
-gem install fastlane --no-rdoc --no-ri --no-document && \
-bundle exec pod install && \
-cd ../node_modules/react-native/third-party/glog-0.3.5 && \
-./../../scripts/ios-configure-glog.sh && \
-cd ../../../../ios && \
-bundle exec fastlane test
