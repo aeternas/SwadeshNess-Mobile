46be6481c8f1cadcb7667ed48d104d623bb8cd65
Ivan Golikov <aeternass@gmail.com>
2018-11-24 12:07:56 +0300

    Added env

--- src/services/TranslationService.js
+++ src/services/TranslationService.js
@@ -1,10 +1,11 @@
 class TranslationService {
-  constructor(props) {
-    this.baseURL = '';
-  }
   translate(string) {
     return fetch(
-      'https://' + baseURL + '/v1/?translate=' + string + '&group=turkic',
+      'https://' +
+        process.env['BASE_URL'] +
+        '/v1/?translate=' +
+        string +
+        '&group=turkic',
     )
       .then(response => response.json())
       .then(responseJson => {
